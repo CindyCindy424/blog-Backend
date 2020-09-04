846bd9d3da9f04b1b5e4f00f4e0409e5f9f99eaf
CindyCindy424 <1909063947@qq.com>
2020-09-04 14:59:55 +0800

    first commit to github,this is the current edition of the blog (without including wxt's code)

---
+++ obj/Debug/netcoreapp3.1/PubTmp/EFSQLScripts/Temperature.Models.blogContext.sql
@@ -0,0 +1,12 @@
+﻿  IF EXISTS(SELECT 1 FROM information_schema.tables 
+  WHERE table_name = '
+'__EFMigrationsHistory'' AND table_schema = DATABASE()) 
+BEGIN
+CREATE TABLE `__EFMigrationsHistory` (
+    `MigrationId` varchar(150) NOT NULL,
+    `ProductVersion` varchar(32) NOT NULL,
+    PRIMARY KEY (`MigrationId`)
+);
+
+END;
+
